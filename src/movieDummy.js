b9f7b09764841aa95bbb4ef4775826c9ff89b372
Suyeon06 <suyeonchae0919@gm.gist.ac.kr>
2026-01-21 23:12:33 +0900

    set api dummy

---
+++ src/movieDummy.js
@@ -0,0 +1,407 @@
+export const dummy = {
+  "dates": {
+    "maximum": "2026-01-21",
+    "minimum": "2025-12-10"
+  },
+  "page": 1,
+  "results": [
+    {
+      "adult": false,
+      "backdrop_path": "/u8DU5fkLoM5tTRukzPC31oGPxaQ.jpg",
+      "genre_ids": [
+        878,
+        12,
+        14
+      ],
+      "id": 83533,
+      "original_language": "en",
+      "original_title": "Avatar: Fire and Ash",
+      "overview": "인간들과의 전쟁으로 첫째 아들 ‘네테이얌’을 잃은 후, ‘제이크’와 ‘네이티리’는 깊은 슬픔에 빠진다. 상실에 빠진 이들 앞에 '바랑'이 이끄는 재의 부족이 등장하면서, 판도라는 더욱 큰 위험에 빠지게 되고, ‘설리’ 가족은 선택의 기로에 서게 되는데…",
+      "popularity": 282.2714,
+      "poster_path": "/l18o0AK18KS118tWeROOKYkF0ng.jpg",
+      "release_date": "2025-12-17",
+      "title": "아바타: 불과 재",
+      "video": false,
+      "vote_average": 7.4,
+      "vote_count": 1451
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/fepnF3W4VP3s8ui8nDtrlsFVYwO.jpg",
+      "genre_ids": [
+        28,
+        9648,
+        18
+      ],
+      "id": 1247002,
+      "original_language": "ko",
+      "original_title": "파과",
+      "overview": "40여 년간 감정 없이 바퀴벌레 같은 인간들을 방역해온 60대 킬러 조각. 대모님이라 불리며 살아있는 전설로 추앙받지만 점차 오랜 시간 몸담은 회사 신성방역에서조차도 한물간 취급을 받는다. 한편, 평생 조각을 쫓은 젊고 혈기 왕성한 킬러 투우는 신성방역의 일원이 되고 조각에게서 시선을 떼지 않는다. 지켜야 할 건 만들지 말자고 약속했던 조각은 여전한 실력에도 불구하고 상처를 입은 그날 밤, 자신을 치료해 준 수의사 강선생과 그의 딸에게 남다른 감정을 느낀다. 투우는 그런 낯선 조각의 모습에 분노가 폭발하는데…",
+      "popularity": 69.2523,
+      "poster_path": "/A9yJjkI7GdqeFYMyUe4PebA2NIw.jpg",
+      "release_date": "2025-04-30",
+      "title": "파과",
+      "video": false,
+      "vote_average": 6.5,
+      "vote_count": 49
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/yCatt8lmp3oRFEcOZF8KHhsiASQ.jpg",
+      "genre_ids": [
+        28,
+        10752
+      ],
+      "id": 1223601,
+      "original_language": "fi",
+      "original_title": "Sisu 2",
+      "overview": "제2차 세계대전이 끝난 직후, 전설적인 핀란드 저격수였던 Aatami Korpi는 고향 집으로 돌아오지만, 가족이 전쟁 중 적군에게 잔인하게 살해된 후다. 그는 집을 해체해 트럭에 싣고 가족을 기리기 위한 새로운 장소에 재건하기로 결심한다. 그런데 가족을 처참히 죽인 소련 붉은 군대 지휘관 Igor Draganov이 그의 뒤를 추적하며, 두 사람 사이에 정면대결과 전국을 가로지르는 추격전이 벌어진다.",
+      "popularity": 63.7757,
+      "poster_path": "/dONN7cHf2MF1nEZX2THgi89otcj.jpg",
+      "release_date": "2026-01-07",
+      "title": "시수: 복수의 길",
+      "video": false,
+      "vote_average": 7.438,
+      "vote_count": 460
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/aHj7d7wSLqrg5cjAcgHhiGr97Ih.jpg",
+      "genre_ids": [
+        28,
+        53,
+        878
+      ],
+      "id": 798645,
+      "original_language": "en",
+      "original_title": "The Running Man",
+      "overview": "실직한 가장 ‘벤 리처즈’가 거액의 상금을 위해 30일간 잔인한 추격자들로부터 살아남아야 하는 글로벌 서바이벌 프로그램에 참가하며 펼쳐지는 추격 액션 블록버스터",
+      "popularity": 56.1894,
+      "poster_path": "/klfSEbFOquMFjBQJ5uKAfp0rrsK.jpg",
+      "release_date": "2025-12-10",
+      "title": "더 러닝 맨",
+      "video": false,
+      "vote_average": 6.817,
+      "vote_count": 978
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/mnEx3lTjjQYHMj55ZnZHQZVUlZ.jpg",
+      "genre_ids": [
+        18,
+        10402,
+        10749
+      ],
+      "id": 1371185,
+      "original_language": "en",
+      "original_title": "Song Sung Blue",
+      "overview": "언제나 자신만의 무대를 꿈꾸지만 현실은 떠돌이 뮤지션 ‘마이크’. 그의 인생에 운명처럼 나타난 싱글맘 ‘클레어’. 첫눈에 서로의 목소리와 마음을 알아본 두 사람은 사랑과 음악을 함께 꿈꾸며 레전드 가수 닐 다이아몬드의 명곡을 부르는 커버 밴드 ‘라이트닝 & 썬더’를 결성한다. 타고난 쇼맨십과 폭발적인 가창력으로 단숨에 지역 스타로 떠오른 두 사람은 당대 최고 인기 밴드 펄잼의 오프닝 무대까지 장식한다. 그러나 가장 찬란한 순간, 예기치 못한 클레어의 사고가 일어나고 두 사람의 인생과 무대는 한순간에 무너지고 마는데....",
+      "popularity": 51.1002,
+      "poster_path": "/ywhIAXy7MbEv3MAfXYXMniGDHBu.jpg",
+      "release_date": "2026-01-14",
+      "title": "송 썽 블루",
+      "video": false,
+      "vote_average": 7.8,
+      "vote_count": 60
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/kVSUUWiXoNwq2wVCZ4Mcqkniqvr.jpg",
+      "genre_ids": [
+        16,
+        10751,
+        35,
+        12,
+        14
+      ],
+      "id": 991494,
+      "original_language": "en",
+      "original_title": "The SpongeBob Movie: Search for SquarePants",
+      "overview": "비키니시티에 사는 스폰지밥은 무시무시한 롤러코스터를 탈 수 있는 용감한 ‘빅 가이’가 꿈이다. 그러던 어느 날, 미지의 세계 ‘언더월드’에 사는 유령선장을 만나 ‘용감무쌍짱 증명서’를 받으면 진정한 ‘빅 가이’가 될 수 있다는 말을 들은 스폰지밥은 절친 뚱이와 함께 해적선을 찾아 위험천만한 모험을 떠난다. 그 사실을 안 집게사장과 징징이, 핑핑이는 스폰지밥을 구하기 위해 따라나서는데…",
+      "popularity": 30.8107,
+      "poster_path": "/2QCGC4toQXYuIsWKj5vJQRAdPjd.jpg",
+      "release_date": "2026-01-01",
+      "title": "스폰지밥 무비: 네모바지를 찾아서",
+      "video": false,
+      "vote_average": 6.493,
+      "vote_count": 67
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/mmd1HnuvAzFc4iuVJcnBrhDNEKr.jpg",
+      "genre_ids": [
+        27,
+        53
+      ],
+      "id": 694,
+      "original_language": "en",
+      "original_title": "The Shining",
+      "overview": "소설가인 잭 토랜스는 콜로라도 산맥에 있는 오버룩 호텔의 관리인으로 취직한다. 다음 시즌 준비를 하는 이곳은 겨울 동안 폐쇄되기 때문에 토랜스의 가족들만 호텔에 머물게 된다. 갑자기 몰아친 폭설로 잭과 아내 웬디와 아들과 함께 호텔에 고립되고, 투시와 텔레파시 능력이 있는 잭의 아들 대니는 호텔에 유령이 있으며 유령들이 천천히 아버지를 미치게 하고 있음을 알게 된다. 잭은 과거에 스스로 자신의 아내와 두 딸을 죽인 이곳의 예전 관리인 그레이디의 유령을 만난다. 과거에 있었던 살인사건의 악령에 휘말린 잭은 호텔을 공포의 도가니로 몰아가는데...",
+      "popularity": 11.7268,
+      "poster_path": "/aaItbIXZlC2C7Arg3PI0IglKi0.jpg",
+      "release_date": "2023-06-28",
+      "title": "샤이닝",
+      "video": false,
+      "vote_average": 8.2,
+      "vote_count": 18488
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/kOUV5WbisWMg56d8eB45ZHEVxYq.jpg",
+      "genre_ids": [
+        18,
+        53,
+        10402
+      ],
+      "id": 1151272,
+      "original_language": "es",
+      "original_title": "Sirāt",
+      "overview": "광활한 사막 한가운데에서 펼쳐지는 레이브 파티. 그곳에서 루이스는 아들 에스테반과 함께 실종된 딸을 찾아 헤맨다. 하지만 곧 그 길은 신의 심판대로 이어지는데...",
+      "popularity": 12.5017,
+      "poster_path": "/wlDPG3NJkXZlTuUrgL0Av1MdHrl.jpg",
+      "release_date": "2026-01-21",
+      "title": "시라트",
+      "video": false,
+      "vote_average": 6.884,
+      "vote_count": 324
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/xSD0q1FiuZkvHuy7uscOLbmd1hR.jpg",
+      "genre_ids": [
+        12,
+        28,
+        14
+      ],
+      "id": 13494,
+      "original_language": "en",
+      "original_title": "Red Sonja",
+      "overview": "고향을 잃은 소녀, 숲이 길러낸 전사. 살아남기 위해 지옥에 뛰어들다. 제국의 침략으로 가족과 고향을 잃은 레드 소냐. 그녀는 야생 속에서 살아남으며 강인한 전사로 다시 태어난다. 제국에 맞선 대가로 포로가 된 소냐는 살아 나올 수 없는 검투장에 던져지고, 그곳에서 억압받던 전사들과 함께 피와 공포로 군림하는 폭군 드레이건에 맞선다. 검투장, 전장, 그리고 신화의 숲까지. 힘이 아닌 자유를 위해 검을 드는 붉은 전사, 레드 소냐.",
+      "popularity": 9.888,
+      "poster_path": "/zLMRIktae700UANwMEhN9mxRm91.jpg",
+      "release_date": "2025-12-31",
+      "title": "레드 소냐",
+      "video": false,
+      "vote_average": 5.747,
+      "vote_count": 168
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/q2V1q2Xxwqg3uXQKufpdCtrnAdn.jpg",
+      "genre_ids": [
+        35,
+        14
+      ],
+      "id": 1114967,
+      "original_language": "en",
+      "original_title": "Good Fortune",
+      "overview": "길 잃은 영혼의 수호천사가 되고 싶지만, 현실은 운전 중 문자하는 인간들을 보호하는 작은 날개의 초짜 천사 가브리엘. 열심히 살아도 차에서 노숙하는 삶을 못 벗어나며 절망에 빠진 N잡러 아지를 구원하기 위해 금수저인 벤처 투자자 제프와 삶을 바꿔준다. 하루아침에 극과 극으로 변한 아지와 제프의 삶은 가브리엘의 의도와 다르게 흘러가고, 이에 대한 책임으로 가브리엘은 날개를 반납하고 인간으로 강등당하는데… 인생이 뒤바뀐 아지와 제프, 인간이 된 가브리엘 이 세 남자의 유쾌한 좌충우돌 운명 재도전기가 펼쳐진다!",
+      "popularity": 12.5763,
+      "poster_path": "/mlpVu3aq1cOjlHOxd70NILlj1fc.jpg",
+      "release_date": "2026-01-07",
+      "title": "굿 포츈",
+      "video": false,
+      "vote_average": 7.1,
+      "vote_count": 397
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/ffQFnAUm2Uu4RU0nijpjPRf9TBT.jpg",
+      "genre_ids": [
+        18,
+        10749
+      ],
+      "id": 843,
+      "original_language": "cn",
+      "original_title": "花樣年華",
+      "overview": "홍콩의 지역 매일 신문 편집장인 초 모완, 수출회사의 비서로 근무하는 수 리첸. 둘은 상하이 지역의 한 건물로 같은 날 이사하게 된다. 이사 날부터 의도치 않게 오가며 자주 부딪히게 되는 두 사람. 둘 다 가정이 있지만 어쩐지 배우자들은 자리를 비우는 날이 더 많고 두 사람의 외로움은 서로에게 낯설지 않게 다가와 둘을 가깝게 한다. 두 사람은 점점 감정이 깊어질수록 겉으로는 더욱 조심스러워지고 예견되어 있는 이별 앞에 마음이 혼란스럽고, 서로의 자리에서 마음으로 바라는 그들의 사랑은 애절하기만 하다.",
+      "popularity": 8.6099,
+      "poster_path": "/mjkr1IamzDiL5mLIbnuhiYOXLqg.jpg",
+      "release_date": "2000-10-20",
+      "title": "화양연화",
+      "video": false,
+      "vote_average": 8.093,
+      "vote_count": 3123
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/6e2yeFZgtD7RKpUYEOjj5v7oAUz.jpg",
+      "genre_ids": [
+        14,
+        18
+      ],
+      "id": 842924,
+      "original_language": "en",
+      "original_title": "The Life of Chuck",
+      "overview": "인터넷은 끊기고, 도로 곳곳은 구멍이 뚫려 마비된 세상. 마치 지구의 마지막 날이 다가오는 것만 같은 나날들의 연속이다. 교사 '마티'는 온 세상이 언제 어둠에 갇혀도 이상할리 없는 지금, 이혼했지만 사랑이 남은 ‘펠리샤’를 만나기 위해 길을 나선다. 그런데 이 절박한 시간에도 거리에서, TV에서, 라디오에서 끊임없이 나오는 누군가를 위한 광고. \"39년 동안의 근사했던 시간, 고마웠어요 척!\" 하지만 주변의 그 누구도 알거나 본 적이 없다는 이 '척'이라는 남자는 마티에게 커다란 궁금증을 남긴다. 그는 누구일까? 세상은 정말 사라져만 가는 것일까?",
+      "popularity": 10.1499,
+      "poster_path": "/pZSZVQH5mlm1EmF3x5O2o14GfTZ.jpg",
+      "release_date": "2025-12-24",
+      "title": "척의 일생",
+      "video": false,
+      "vote_average": 7.314,
+      "vote_count": 717
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/xxFH8OgUVw5LyJmqBaXeV97mfri.jpg",
+      "genre_ids": [
+        35,
+        18
+      ],
+      "id": 1159206,
+      "original_language": "en",
+      "original_title": "Father Mother Sister Brother",
+      "overview": "미국 북동부, 아일랜드 더블린, 프랑스 파리. 거리만큼 관계도 멀어진 세 가족의 오랜만의 만남을 섬세하고 우아하게 담은 작품.",
+      "popularity": 8.6655,
+      "poster_path": "/jid3LHJM91QdHrYFtghsJDLg2HM.jpg",
+      "release_date": "2025-12-31",
+      "title": "파더 마더 시스터 브라더",
+      "video": false,
+      "vote_average": 6.788,
+      "vote_count": 99
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/5ziZTaQ7zb3IUMkw6IEimFktNMS.jpg",
+      "genre_ids": [
+        10749,
+        18
+      ],
+      "id": 1264805,
+      "original_language": "ko",
+      "original_title": "만약에 우리",
+      "overview": "고향 가는 고속버스에 올라탄 은호와 휴학 후 어디론가 떠날 결심을 한 정원, 나란히 앉게 된 두 사람은 뜻밖의 인연을 맺는다. 서로의 꿈을 응원하며 의지하던 두 사람은 어느새 일상 깊숙이 스며들어 연인으로 발전한다. 웃고, 싸우고, 화해하며 세상을 다 가진 듯 뜨겁게 사랑하지만, 현실의 벽 앞에 두 사람은 결국 다른 길을 선택하게 된다. 그렇게 10년이 지난 후, 다시 마주한 순간 은호는 정원에게 오랫동안 묻어두었던 한마디를 꺼낸다.",
+      "popularity": 8.5894,
+      "poster_path": "/9C01XqrH6kyat5gq01tKyipfni3.jpg",
+      "release_date": "2025-12-31",
+      "title": "만약에 우리",
+      "video": false,
+      "vote_average": 8,
+      "vote_count": 1
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/8SaEH4kYCy7JlviyhKtSVsMkt4r.jpg",
+      "genre_ids": [
+        28,
+        53,
+        12
+      ],
+      "id": 1315988,
+      "original_language": "es",
+      "original_title": "Mikaela",
+      "overview": "전례 없는 눈폭풍이 스페인 전역을 강타하는 밤, 주요 고속도로가 폐쇄되고 모든 교통과 통신이 마비되는 대혼란이 발생한다. 수년간의 경찰 생활 끝에 막다른 길에 몰린 베테랑 경찰 레오는 이 눈보라 속에 갇혀 우연히 한 사건에 휘말린다. 바로 이 극심한 날씨를 틈타 중무장한 강도단이 현금 수송차를 습격하는 현장을 목격하게 된다. 레오는 고립된 고속도로에서 강도단을 막기 위해 홀로 분투하지만 역부족이다. 이때, 우연히 현장에 있던 미카엘라(나탈리아 아사하라)라는 이름의 젊은 예비 경찰이 레오에게 예상치 못한 도움을 다. 두 사람은 혹독한 추위와 강도단의 위협 속에서, 눈폭풍이 휩쓰는 멈춰버린 고속도로를 무대로 강도들을 저지하고 현금을 되찾기 위한 필사적이고 치열한 추격전을 벌인다.",
+      "popularity": 7.2412,
+      "poster_path": "/fA2KfSTChxQl2GAtMYx3DyJ8Eah.jpg",
+      "release_date": "2025-12-18",
+      "title": "스노우폴",
+      "video": false,
+      "vote_average": 6.1,
+      "vote_count": 100
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/wmTBmCLPZSX5eAVAiBf8td8Hb9g.jpg",
+      "genre_ids": [
+        16,
+        35,
+        10751
+      ],
+      "id": 1497970,
+      "original_language": "zh",
+      "original_title": "猫和老鼠：星盘奇缘",
+      "overview": "세기의 콤비 톰과 제리, 최초 3D 탄생! 마법의 나침반을 깨운 톰과 제리의 차원이동이 시작된다! 박물관 경비원으로 일하는 톰은 몰래 들어온 제리를 쫓다 우연히 마법의 황금나침반을 깨우게 되고, 신비로운 빛에 휩싸여 동양의 한 고대도시에 불시착한다 뜻밖의 차원이동을 하게 된 톰과 제리는 300년 동안 고대도시를 지켜온 봉황사부와 그의 친구들을 만나 기상천외한 모험을 시작하는데…! 황금나침반을 이용해 천계로 돌아가야 하는 봉황사부와 톰과 제리의 시공을 초월한 모험이 펼쳐진다!",
+      "popularity": 8.8712,
+      "poster_path": "/fwtu3bazam69kAIlzilf4irGroX.jpg",
+      "release_date": "2026-01-01",
+      "title": "톰과 제리: 황금나침반 대소동",
+      "video": false,
+      "vote_average": 4.3,
+      "vote_count": 6
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/j2ZvLJyz163MlmBFsoaDYOwxgws.jpg",
+      "genre_ids": [
+        12,
+        14,
+        16,
+        28,
+        10751
+      ],
+      "id": 10515,
+      "original_language": "ja",
+      "original_title": "天空の城ラピュタ",
+      "overview": "광산촌 슬랙 계곡의 기계 견습공인 고아 소년 파즈는 어느날 빛이 나는 목걸이를 한 채 하늘에서 떨어지던 소녀시타를 구해준다. 소녀는 대대로 전해져 오던 목걸이(비행석)로 인해 정부군과 해적 도라 일당들에게 쫓기고 있던 신세. 시타가 이들로부터 무사히 도망갈 수 있게 도와주던 중 파즈는 비행석과 하늘에 떠 있는 성 라퓨타가 실제로 존재한다는 사실을 알게 된다. 라퓨타의 존재를 믿고 있던 파즈는 시타와 함께 라퓨타를 찾기로 결심한다. 하지만 파즈와 시타는 그들을 쫓던 군대에게 잡히고, 시타는 정부 비밀 조사관인 무스카에게 파즈를 풀어주는 조건으로 협력을 약속하는데...",
+      "popularity": 6.2254,
+      "poster_path": "/lObRXt53izxDbdlkGpNAHkDACmy.jpg",
+      "release_date": "2004-04-30",
+      "title": "천공의 성 라퓨타",
+      "video": false,
+      "vote_average": 7.966,
+      "vote_count": 4544
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/69D57G3XqQMVNCwK0Z2PCcYC4Da.jpg",
+      "genre_ids": [
+        18
+      ],
+      "id": 25538,
+      "original_language": "zh",
+      "original_title": "一一",
+      "overview": "8살 소년 양양은 아빠 NJ로부터 카메라를 선물 받는다. 사람들이 보지 못하는 그들의 뒷모습을 찍는 양양. 양양의 사진 속에는 사업이 위기에 빠진 시기에 30년 전 첫사랑을 다시 만나게 된 아빠 NJ, 외할머니가 사고로 쓰러진 뒤 슬픔에 빠져 집을 떠나있게 된 엄마 민민, 외할머니의 사고가 자신 때문이라고 생각하는 누나 팅팅 그리고 저마다의 삶을 살아가는 사람들이 있다.",
+      "popularity": 6.5016,
+      "poster_path": "/qdx6yQJ8WLLyBnwLcdxMznIg4bB.jpg",
+      "release_date": "2000-10-28",
+      "title": "하나 그리고 둘",
+      "video": false,
+      "vote_average": 7.9,
+      "vote_count": 631
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/c15Fg4jvclF5VjxCi1mtjUg4a6H.jpg",
+      "genre_ids": [
+        35,
+        18,
+        36
+      ],
+      "id": 1254808,
+      "original_language": "fr",
+      "original_title": "Nouvelle Vague",
+      "overview": "1959년 파리, 젊은 비평가 ‘고다르’가 올해 최악의 영화이자 세기의 데뷔작이 될 <네 멋대로 해라>를 찍는 미친 모험의 시간으로 점프하는 리처드 링클레이터의 시네마 매직",
+      "popularity": 5.1463,
+      "poster_path": "/y5UUa7KSStUar4rdCxvclOj3qvB.jpg",
+      "release_date": "2025-12-31",
+      "title": "누벨바그",
+      "video": false,
+      "vote_average": 7.376,
+      "vote_count": 93
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/xmSkUXvYFQyMnxTUJJtEVvpSShk.jpg",
+      "genre_ids": [
+        16,
+        10402,
+        12,
+        35
+      ],
+      "id": 1404404,
+      "original_language": "ja",
+      "original_title": "クレヨンしんちゃん 超華麗！灼熱のカスカベダンサーズ",
+      "overview": "떡잎마을 어린이 엔터 페스티벌에서 우승한 떡잎마을 방범대 짱구, 맹구, 철수, 유리, 훈이는 우승 상품으로 인도 엔터 페스티벌 무대에 초청받아, 여행을 만끽한다. 짱구와 맹구는 수상한 잡화점에서 코 모양처럼 생긴 배낭을 발견하고, 맹구는 배낭의 콧구멍에 꽂혀 있는 휴지(?)를 홀린 듯이 자신의 코에 꽂아버린다. 그 순간, 알 수 없는 힘이 맹구에게서 뿜어져 나오는데…! 겉보기에는 평범한 휴지 같은 이 종이의 비밀은 바로, 종이를 코에 꽂는 사람에게 욕망을 불러일으켜 무자비하고 제멋대로인 폭군으로 만들어 버리는 것. “나에 대해 다 아는 척하지 마” 순둥이 맹구, 흑화해서 폭군으로 대변신! 폭주를 시작한다!",
+      "popularity": 5.7155,
+      "poster_path": "/hh2eWvm91RMIVQb0esjQab8sWgh.jpg",
+      "release_date": "2025-12-24",
+      "title": "극장판 짱구는 못말려: 초화려! 작열하는 떡잎마을 댄서즈",
+      "video": false,
+      "vote_average": 7.3,
+      "vote_count": 3
+    }
+  ],
+  "total_pages": 5,
+  "total_results": 86
+}
